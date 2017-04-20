97464ff7c02e162f7bd7a0652e94711817623720
Double0207 <1056737159@qq.com>
2017-04-20 20:39:10 +0800

    小整

--- public/js/zy-index.js
+++ public/js/zy-index.js
@@ -1,14 +1,62 @@
-/**
- * Created by Administrator on 2017/4/19.
- */
-var dialogModal=document.getElementById("dialogModal");
-var upload=document.getElementById("upload");
-var close=document.getElementById("close");
+// /**
+//  * Created by Administrator on 2017/4/19.
+//  */
+// var dialogModal=document.getElementById("dialogModal");
+
+// var showdialogModal;
+// var closedialogModal;
+//上传照片
+function showdialogModal() {
+    document.getElementById("dialogModal").style.display="block";
+}
+function closedialogModal(){
+    document.getElementById("dialogModal").style.display="none";
+}
 
 
-upload.onclick=function(){
-    dialogModal.style.display="block";
+//购物车
+function updateTotal() {
+    var items = $('div.shopping-cart-items:first div.item');
+    $('#cart-total-box .total-items span').text(items.length);
+    var total = 0;
+    items.each(function(){total += parseInt($(this).find('.price-money span').text(), 10);});
+    $('#cart-total-box h3 span').text(total);
 }
-close.onclick=function(){
-    dialogModal.style.display="none";
+function removeCartItem(btn) {
+    btn = $(btn);
+    var item = btn.parents('div.item:first');
+    item.hide();
+    $.post('shopping-cart/remove-item', {request: item.attr('request'), product: item.attr('product')}, function(response){
+        item.appendTo('div.shopping-cart-items.removed');
+        item.show();
+        showFlashMessage(response);
+        updateTotal();
+        $('div.shopping-cart-items.removed > h3').show();
+    });
 }
+function restoreCartItem(btn) {
+    btn = $(btn);
+    var item = btn.parents('div.item:first');
+    item.hide();
+    $.post('shopping-cart/add-item', {request: item.attr('request'), product: item.attr('product')}, function(response){
+        item.appendTo('div.shopping-cart-items:first');
+        item.show();
+        showFlashMessage(response);
+        updateTotal();
+        if (! $('div.shopping-cart-items.removed div.item').length) {
+            $('div.shopping-cart-items.removed > h3').hide();
+        }
+    });
+}
+$(document).ready(function(){
+    $(window).scroll(function(){
+        var b = $('#cart-total-box');
+        if (! b) return;
+        if (b.parent().width() == b.parent().prev().width()) return true;
+        var s = $(window).scrollTop();
+        if (s < b.parent().prev().height()) b.css('margin-top', s);
+    });
+});
+
+
+
